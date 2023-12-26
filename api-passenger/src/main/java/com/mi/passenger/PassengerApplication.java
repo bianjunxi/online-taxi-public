4b441871f461c924adf70f854cb8fb889364b52f
Jay <bjxstart@gmail.com>
2023-12-26 22:12:45 +0800

    api-passenger项目骨架搭建

---
+++ api-passenger/src/main/java/com/mi/passenger/PassengerApplication.java
@@ -0,0 +1,18 @@
+package com.mi.passenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: PassengerApplication
+ * Description:
+ *
+ * @author Leo
+ * @version v1.0
+ */
+@SpringBootApplication
+public class PassengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerApplication.class, args);
+    }
+}
---
+++ api-passenger/src/main/java/com/mi/passenger/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.mi.passenger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: TestController
+ * Description:
+ *
+ * @author Leo
+ * @version v1.0
+ */
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test api passenger";
+    }
+}
