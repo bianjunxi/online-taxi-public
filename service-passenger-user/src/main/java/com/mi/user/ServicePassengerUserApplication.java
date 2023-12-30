79d5d54705d70b7779e8753f57a9f9af28516e31
bianjunxi <junxibian@163.com>
2023-12-30 10:59:36 +0800

    创建项目service-passenger-user

---
+++ service-passenger-user/src/main/java/com/mi/user/ServicePassengerUserApplication.java
@@ -0,0 +1,18 @@
+package com.mi.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: ServicePassengerUserApplication
+ * Description:
+ *
+ * @author Jay
+ * @version v1.0
+ */
+@SpringBootApplication
+public class ServicePassengerUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePassengerUserApplication.class, args);
+    }
+}
