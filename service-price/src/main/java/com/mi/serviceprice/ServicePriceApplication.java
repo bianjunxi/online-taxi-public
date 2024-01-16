9dfb527f71806cd87ee242e916e45bd52f0e0e22
bianjunxi <junxibian@163.com>
2024-01-16 22:38:43 +0800

    预估价格服务骨架代码编写完成

---
+++ service-price/src/main/java/com/mi/serviceprice/ServicePriceApplication.java
@@ -0,0 +1,18 @@
+package com.mi.serviceprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName:  ServicePriceApplication
+ * Description: ServicePrice启动类
+ *
+ * @author Jay
+ * @version v1.0
+ */
+@SpringBootApplication
+public class ServicePriceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePriceApplication.class, args);
+    }
+}
---
+++ service-price/src/main/java/com/mi/serviceprice/service/ForecastPriceService.java
@@ -0,0 +1,22 @@
+package com.mi.serviceprice.service;
+
+import com.mi.common.dto.ResponseResult;
+import com.mi.common.request.ForecastPriceDto;
+
+/**
+ * ClassName:  ForecastPriceService
+ * Description: 预估价格
+ *
+ * @author Jay
+ * @version v1.0
+ */
+public interface ForecastPriceService {
+
+    /**
+     * 根据出发地和目的地经纬度 计算价格
+     * @param dto 请求参数封装经纬度
+     * @return ResponseResult
+     */
+    ResponseResult forecastPrice(ForecastPriceDto dto);
+
+}
